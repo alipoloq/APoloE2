4fb050bf0723d7266e1778fd359f514bce1dd992
alipoloq <a_polo1706@hotmail.com>
2023-01-12 19:57:21 -0500

    Creación del proyecto

---
+++ citas-react/src/App.jsx
@@ -0,0 +1,12 @@
+/*Funcion creadora de la aplicación*/
+function App() {
+  /*estado de retorno constante*/
+  return (
+    /*Etiqueta padre y el body de la página*/
+    <>
+     
+    </>
+  )
+}
+/*Export para importar el módulo a otras partes de la app*/
+export default App
